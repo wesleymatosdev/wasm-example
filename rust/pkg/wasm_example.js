03b63edd1d511b71286d2f9336b77fd61b1d6543
Wesley Matos <wesleymatosdev@gmail.com>
2025-10-31 19:39:43 -0300

    build: use nodejs target instead of bundler

--- rust/pkg/wasm_example.js
+++ rust/pkg/wasm_example.js
@@ -1,5 +1,57 @@
-import * as wasm from "./wasm_example_bg.wasm";
-export * from "./wasm_example_bg.js";
-import { __wbg_set_wasm } from "./wasm_example_bg.js";
-__wbg_set_wasm(wasm);
+
+let imports = {};
+imports['__wbindgen_placeholder__'] = module.exports;
+
+let cachedUint8ArrayMemory0 = null;
+
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+function decodeText(ptr, len) {
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
+}
+
+function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return decodeText(ptr, len);
+}
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+exports.add = function(a, b) {
+    const ret = wasm.add(a, b);
+    return ret;
+};
+
+exports.__wbg_log_d0923b42abf02859 = function(arg0, arg1) {
+    console.log(getStringFromWasm0(arg0, arg1));
+};
+
+exports.__wbindgen_init_externref_table = function() {
+    const table = wasm.__wbindgen_externrefs;
+    const offset = table.grow(4);
+    table.set(0, undefined);
+    table.set(offset + 0, undefined);
+    table.set(offset + 1, null);
+    table.set(offset + 2, true);
+    table.set(offset + 3, false);
+    ;
+};
+
+const wasmPath = `${__dirname}/wasm_example_bg.wasm`;
+const wasmBytes = require('fs').readFileSync(wasmPath);
+const wasmModule = new WebAssembly.Module(wasmBytes);
+const wasm = exports.__wasm = new WebAssembly.Instance(wasmModule, imports).exports;
+
 wasm.__wbindgen_start();
+
